24adfdb75a48069850723d5a932db4728962dfb6
tchabanamalia <tchabanamalia@gmail.com>
2022-11-30 11:38:04 +0000

    Premier commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <style>
+        .img-custom{
+          height: 500px !important;
+          object-fit: cover;
+        }
+      </style>
+      
+      <body>       
+        <h2 class="text-secondary">LARAVEL-FIREBASE</h2>   
+      </body>
+
+</body>
+</html>
